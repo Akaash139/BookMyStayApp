27be9ee85c597b6da48390908b59d440d39cc3ac
Akaash139 <akaash.paran@gmail.com>
2026-03-17 15:10:14 +0530

    Akaash added UC1

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,42 @@
+/**
+ * ============================================================
+ * MAIN CLASS - UseCase1HotelBookingApp
+ * ============================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * Hotel Booking Management System.
+ *
+ * At this stage, the application:
+ * - Starts execution from the main() method
+ * - Displays a welcome message
+ * - Shows application version
+ *
+ * No business logic is implemented yet.
+ *
+ * The goal is to establish a clear startup flow.
+ *
+ * @author Akaash
+ * @version 1.0
+ */
+
+public class BookMyStayApp{
+
+    /**
+     * Application entry point.
+     *
+     * This is the first method executed by the JVM
+     * when the program starts.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println(" Welcome to the Hotel Booking System ");
+        System.out.println("=======================================");
+        System.out.println("System initialized successfully.");
+    }
+}
